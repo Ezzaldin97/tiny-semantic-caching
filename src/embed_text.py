7a2df977f8c0c249bd474d47977a41e92b619742
Ezzaldin97 <kingtal632@yahoo.com>
2024-05-31 22:38:38 +0300

    updating ollama host

--- src/embed_text.py
+++ src/embed_text.py
@@ -7,10 +7,12 @@ import ollama
 class Embed:
     def __init__(self) -> None:
         self._model = os.getenv("ollama_model")
+        self._host = os.getenv("host")
 
     def embed(self, text: str) -> List[float]:
         if isinstance(text, str):
-            result = ollama.embeddings(
+            client = ollama.Client(host=self._host)
+            result = client.embeddings(
                 model="nomic-embed-text",
                 prompt=text,
             )
