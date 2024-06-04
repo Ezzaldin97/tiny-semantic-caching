21f10f794151ac8d632856bc752cc5e6048588ff
Ezzaldin97 <kingtal632@yahoo.com>
2024-06-05 01:06:37 +0300

    document API functionalities

--- src/base.py
+++ src/base.py
@@ -6,15 +6,23 @@ from fastapi import APIRouter
 from src.embed_text import Embed
 from src.vector_db import VectorStore
 
+# Create an APIRouter instance with a prefix and tags
 router = APIRouter(
     prefix="/api",
     tags=["semantic-caching"],
 )
+
+# Initialize a VectorStore instance
 vs = VectorStore()
 
 
 @router.get("/")
 async def root():
+    """
+    Root endpoint to check the health of the application.
+    
+    Returns a JSON response with a message and application details.
+    """
     return {
         "message": "app is healthy, and safely running",
         "data": {
@@ -26,6 +34,14 @@ async def root():
 
 @router.get("/vectorize/{text}")
 async def vectorize(text: str):
+    """
+    Endpoint to vectorize input text using an Embed instance.
+    
+    Args:
+    text (str): The input text to be vectorized.
+    
+    Returns a JSON response with a message and the vectorized result.
+    """
     embedder = Embed()
     result = embedder.embed(text)
     return {"message": "embeddings created successfully", "data": {"response": result}}
@@ -35,6 +51,16 @@ async def vectorize(text: str):
 async def insert_embedding(
     text: str, embedding: List[float], metadata: Optional[Dict[str, Any]] = None
 ):
+    """
+    Endpoint to insert a vectorized text into the vector database.
+    
+    Args:
+    text (str): The input text.
+    embedding (List[float]): The vectorized representation of the input text.
+    metadata (Optional[Dict[str, Any]], optional): Additional metadata for the input text. Defaults to None.
+    
+    Returns a JSON response with a message indicating the insertion status.
+    """
     vs.insert(text, embedding, metadata)
     return {
         "message": "data saved in vector database",
@@ -44,6 +70,14 @@ async def insert_embedding(
 
 @router.post("/search/{text}")
 async def search(text: str):
+    """
+    Endpoint to search for similar texts in the vector database.
+    
+    Args:
+    text (str): The input text to search for.
+    
+    Returns a JSON response with a message, the search result, and the distance score.
+    """
     embeddings_result = await vectorize(text)
     ## search first
     results_df = vs.search(embeddings_result["data"]["response"])
@@ -56,14 +90,16 @@ async def search(text: str):
         result = result["text"].tolist()[0]
     ### must be saved directly
     await insert_embedding(text, embeddings_result["data"]["response"])
-    return {
-        "message": "data saved in vector database",
-        "data": {"response": result, "score": score},
-    }
+    return {"message": "data saved in vector database", "data": {"response": result, "score": score}}
 
 
 @router.delete("/refresh/")
 async def refresh():
+    """
+    Endpoint to refresh the vector database.
+    
+    Returns a JSON response with a message indicating the refresh status.
+    """
     vs.refresh()
     return {
         "message": "database refreshed",
