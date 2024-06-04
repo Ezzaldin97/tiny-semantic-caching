6fcc630de4c68454babc2a018c3c07378394bc85
Ezzaldin97 <kingtal632@yahoo.com>
2024-06-05 01:19:47 +0300

    document fastapi main

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
-from dotenv import load_dotenv
+from fastapi import FastAPI  # Import the FastAPI class
+from dotenv import (
+    load_dotenv,
+)  # Import the load_dotenv function to load environment variables from a.env file
 
+# Load environment variables from a.env file
 load_dotenv(".env")
+
+# Import the base module from the src package
 from src import base
 
+# Create a FastAPI application instance
 app = FastAPI()
+
+# Include the router from the base module into the FastAPI application
 app.include_router(base.router)
