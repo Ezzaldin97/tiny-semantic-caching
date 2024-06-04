7be5b44459b840d0a5a3993a4dae9ccf51464326
Ezzaldin97 <kingtal632@yahoo.com>
2024-06-05 01:39:32 +0300

    handle non-existance of embedding model

--- src/embed_text.py
+++ src/embed_text.py
@@ -9,12 +9,36 @@ class Embed:
     def __init__(self) -> None:
         self._model = os.getenv("ollama_model")
         self._host = os.getenv("host")
+        self.client = ollama.Client(host=self._host)
+        ## preparation
+        self._prepare()
+
+    def _prepare(self) -> None:
+        try:
+            models = self.client.list()
+            models_lst = [
+                model['name'] for model in models['models'] 
+            ]
+            if len(models['models'])==0:
+                self.client.pull(self._model)
+            else:
+                if self._model not in models_lst:
+                    self.client.pull(self._model)
+        except ConnectError:
+            models = ollama.list()
+            models_lst = [
+                model['name'] for model in models['models'] 
+            ]
+            if len(models['models'])==0:
+                ollama.pull(self._model)
+            else:
+                if self._model not in models_lst:
+                    ollama.pull(self._model)
 
     def embed(self, text: str) -> List[float]:
         if isinstance(text, str):
             try:
-                client = ollama.Client(host=self._host)
-                result = client.embeddings(
+                result = self.client.embeddings(
                     model="nomic-embed-text",
                     prompt=text,
                 )
